b93e2979958feade78e0b465864b743981deb23f
roberthoog <minadress@gmail.com>
2019-03-05 00:54:35 +0100

    Pushing changes in Gulp

--- wp-content/themes/blankslate/gulpfile.js
+++ wp-content/themes/blankslate/gulpfile.js
@@ -16,18 +16,48 @@ gulp.task("sass", function () {
         includePaths: [bourbon]
     }))
     //.pipe(autoprefix("last 2 versions"))
-    .pipe(gulp.dest("./assets/css"))
+    .pipe(gulp.dest("./assets/css/styles.css"))
    ;
 });
 
+browserSync
+// watch PHP files change
+var src = {
+   php:'./wp-content/**/*.php',
+};
 
-// Static server
+
+gulp.task('php', function(){
+    gulp.src(src.php)
+    .pipe(reload({stream:true}));
+});
+
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+     
+ }
+    });
+});
+
+
+gulp.task('BrowerSync', function() {
+    BrowerSync.init({
+        proxy: "localhost:666"
+    });
+});
+
 gulp.task('browserSync', function() {
     browserSync.init({
         server: { localhost:666,
-            baseDir: "./"
+            baseDir: "http://localhost:666/iw/",
+                target: "localhost:666", // can be [virtual host, sub-directory, localhost with port]
+
         }
     });
+
 });
 
 gulp.task("default", ["sass", "browserSync"], function() {
--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -12,15 +12,11 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div id="wrapper" class="hfeed">
-<header id="header">
-<div id="branding">
-<div id="site-title">
+
 <?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?>
 <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
 <?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?>
 </div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
 </div>
 <nav id="menu">
 <div id="search"><?php get_search_form(); ?></div>
